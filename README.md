c7d111814d959dc902ec15190baa7583f3802439
juanjovita <juanjovita22@gmail.com>
2022-03-29 14:18:46 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-2pjuq4
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-2pjuq4)
---
+++ index.js
@@ -0,0 +1,24 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+const appDiv = document.getElementById('app');
+appDiv.innerHTML = `<h1>JS Starter</h1>`;
+
+const numeroAdivinhacao = 7;
+let chute = "";
+let acertou = false;
+
+for(let contador = 0; contador < 3; contador++){    
+    chute = prompt("Tente adivinhar o número de 0 a 10:");
+    if(chute == numeroAdivinhacao){
+        alert(`Parabéns, você acertou! O número era ${numeroAdivinhacao}.`);
+        acertou = true;
+        break;
+    }
+    alert("Errado!");
+}
+if(!acertou){
+    alert(`Infelizmente, você não acertou. O número era ${numeroAdivinhacao}!`);
+}
+ 
